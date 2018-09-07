df3e49cb1608c7d9d5abd3bf07ecb19568cc7b5e
phjung <pyamont@gmail.com>
2018-09-06 22:16:17 -0400

    update code

--- R/HCEB.moran.R
+++ R/HCEB.moran.R
@@ -1,39 +1,54 @@
 #' Global Heteroscedastic Consistent Empirical Bayes Moran's I estimator
 #'
-#' Compute Global HC-EB Moran's I. Global Moran's I with each case weighted by HC-EB method.
-#' @param x a numeric vector of counts of cases
-#' @param n a numeric vector of populations at risk
-#' @param k a numeric vector of margin of errors of x
-#' @param b default 0, base populations-at-risk
+#' Compute Global HC-EB Moran's I. Global Moran's I of areal prevalence rates with uncertainty in denominator.
+#' @param n a numeric vector of counts of cases (numerator)
+#' @param x a numeric vector of populations at risk (denominator)
+#' @param se a numeric vector of sampling standard errors of corresponding populations at risk
 #' @param listw a listw object created for example by nb2listw
 #' @param zero.policy default NULL, use global option value; if TRUE assign zero to the lagged value of zones without neighbours, if FALSE assign NA
-#' @references #####, ##### and #####. 2018. Small area adjustment of spatial autocorrelation estimators for heteroscedastic sampling errors, Geographical Analysis (under review)
+#' @references Jung, PH, Thill J-C, Issel M 2018 Spatial Autocorrelation Statistics of Areal Prevalence Rates under High Uncertainty in Denominator Data, Geographical Analysis
 #' @keywords HCEB
 #' @export
 # @examples
 # HCEB.est()
 
-HCEB.moran <- function (x, n, k, b=0, listw, zero.policy = NULL)
+HCEB.moran <- function (n, x, se, listw, zero.policy = NULL)
 {
+  if (!is.numeric(n))
+    stop(paste(deparse(substitute(n)), "is not a numeric vector"))
+  if (!is.numeric(x))
+    stop(paste(deparse(substitute(x)), "is not a numeric vector"))
+  if (!is.numeric(se))
+    stop(paste(deparse(substitute(se)), "is not a numeric vector"))
+  if (any(is.na(n)))
+    stop("NA in cases")
+  if (any(is.na(x)))
+    stop("NA in at risk population")
+  if (any(is.na(se)))
+    stop("NA in stadard errors of risk population")
+  if (any(n < 0))
+    stop("negative number of cases")
+  if (any(x < 0))
+    stop("negative number of risk population")
+  if (length(x) != length(n) | length(n) != length(se) | length(x) != length(se))
+    stop("vectors of different length")
   if (is.null(zero.policy))
-    zero.policy <- get("zeroPolicy", envir = .spdepOptions)
+    zero.policy <- get("zeroPolicy")
   stopifnot(is.logical(zero.policy))
+
   n1 <- length(listw$neighbours)
-  if (n1 != length(x) | n1!=length(n))
+  if (n1 != length(x))
     stop("objects of different length")
 
-  r <- x/(b+n)  # raw rate
-  N = sum(b+n)  # sum of the population-at-risk
-  m = sum(x)/N  # global average rate
+  d <- HCEB.est(n, x, se)
+  m <- attributes(d)$m
 
-  z <- HCEB.est(x, n, k, b)$HCEB-m
+  z <- d$HCEB-m
   lz <- lag.listw(listw, z, zero.policy = zero.policy)
   zz <- sum(z^2)
 
   I <- (sum(z * lz))/zz
-
   res <- I
 
   res
 }
-environment(HCEB.moran) <- asNamespace("spdep")
--- R/HCEB.moran.mc.R
+++ R/HCEB.moran.mc.R
@@ -1,23 +1,22 @@
 #' Permutation test for Global Heteroscedastic Consistent Empirical Bayes Moran's I
 #'
-#' A permutation test for Global HC-EB Moran's I statistics by using nsim random permutations of HC-EB rate.
-#' @param x a numeric vector of counts of cases
-#' @param n a numeric vector of populations at risk
-#' @param k a numeric vector of margin of errors of x
-#' @param b default 0, base populations-at-risk
+#' A permutation test for Global HC-EB Moran's I statistics by using nsim random permutations of HC-EB rates.
+#' @param n a numeric vector of counts of cases (numerator)
+#' @param x a numeric vector of populations at risk (denominator)
+#' @param se a numeric vector of sampling standard errors of corresponding populations at risk
 #' @param listw a listw object created for example by nb2listw
+#' @param zero.policy default NULL, use global option value; if TRUE assign zero to the lagged value of zones without neighbours, if FALSE assign NA
 #' @param sig default 0.05, pseudo significance level for permutation test
 #' @param nsim number of permutations
-#' @param zero.policy default NULL, use global option value; if TRUE assign zero to the lagged value of zones without neighbours, if FALSE assign NA
-#' @references #####, ##### and #####. 2018. Small area adjustment of spatial autocorrelation estimators for heteroscedastic sampling errors, Geographical Analysis (under review)
+#' @references Jung, PH, Thill J-C, Issel M 2018 Spatial Autocorrelation Statistics of Areal Prevalence Rates under High Uncertainty in Denominator Data, Geographical Analysis
 #' @keywords HCEB
 #' @export
 # @examples
 # HCEB.est()
 
-HCEB.moran.mc <- function(x, n, k, b=0, listw, sig = 0.05, nsim, zero.policy = NULL)
+HCEB.moran.mc <- function(n, x, se, zero.policy = NULL, listw, sig = 0.05, nsim = 1000)
 {
-  I.global <- HCEB.moran(x, n, k, b, listw, zero.policy)
+  I.global <- HCEB.moran(n, x, se, listw, zero.policy)
   n1 <- length(listw$neighbours)
   I.perm <- numeric(length = nsim)
 
@@ -29,8 +28,7 @@ HCEB.moran.mc <- function(x, n, k, b=0, listw, sig = 0.05, nsim, zero.policy = N
   }
   p.perm <- sum(t(I.perm)>I.global)/nsim
 
-  res <- list("I"=I.global, "p.perm"=p.perm)
+  res <- list("I_HCEB"=I.global, "p_perm"=p.perm)
 
   res
 }
-environment(HCEB.moran.mc) <- asNamespace("spdep")
